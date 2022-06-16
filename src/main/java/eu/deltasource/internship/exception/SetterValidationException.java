20562760db3cf894725c3888540758f2d53006a4
Plamen Gospodinov <armageddon16@abv.bg>
2022-06-16 16:40:13 +0300

    Check commit from new mac

--- src/main/java/eu/deltasource/internship/exception/SetterValidationException.java
+++ src/main/java/eu/deltasource/internship/exception/SetterValidationException.java
@@ -2,6 +2,7 @@ package eu.deltasource.internship.exception;
 
 public class SetterValidationException extends IllegalArgumentException {
 
+    //Some comment
         public SetterValidationException(String argument) {
             super("Enter valid " + argument + " !");
         }
